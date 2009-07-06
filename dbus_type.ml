ac5bb238d34b4c087feeae0d46c3d4231da4f5a3
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2009-07-05 19:17:55 -0700

    fix padding computation

--- dbus_type.ml
+++ dbus_type.ml
@@ -75,8 +75,11 @@ let alignment_of = function
   | T_array _ ->            4
   | T_struct _ ->           8
 
-let get_padding ~offset:current_offset ~align:alignment =
-  alignment - (current_offset mod alignment)
+let get_padding ~offset ~align =
+  let ofs = offset mod align in
+    match ofs with
+      | 0 -> 0
+      | _ -> align - ofs
 
 type endian =
   | Little_endian
